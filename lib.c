fbb1d4d6488403ffdfb2f72775d07862f430ccb6
nickdrozd <nicholasdrozd@gmail.com>
2016-07-25 13:16:43 -0400

    added cons, car, cdr, nil to library

--- lib.c
+++ lib.c
@@ -1,9 +1,30 @@
+#define cons \
+	"(define cons \
+		(lambda (x y) \
+			(lambda (s) \
+				(s x y))))"
+
+#define car \
+	"(define car \
+		(lambda (p) \
+			(p (lambda (x y) \
+					x))))"
+
+#define cdr \
+	"(define cdr \
+		(lambda (p) \
+			(p (lambda (x y) \
+					y))))"
+
+#define nil \
+	"(define nil (quote ()))"
+
 #define fact_rec \
 	"(define recursive_factorial \
 		(lambda (n) \
 			(if (= n 0) \
 				1 \
-				(* n (recursive_factorial (- n 1))))))\n"
+				(* n (recursive_factorial (- n 1))))))"
 
 #define fact_iter \
 	"(define iterative_factorial \
@@ -15,10 +36,17 @@
 							total \
 							(loop (- count 1) \
 								(* total count))))) \
-				(loop n 1))))\n"
+				(loop n 1))))"
 
+
+char* library[] = {cons"\n",
+					 car"\n", 
+					 cdr"\n", 
+					 nil"\n", 
+					 fact_rec"\n", 
+					 fact_iter"\n"};
 
 /* lib_len should match the length of library
-	(this has to be handled manually) */
-char* library[] = {fact_rec, fact_iter};
-int lib_len = 2;
+	(this has to be handled manually?) */
+
+int lib_len = 6;
