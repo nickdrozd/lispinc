291348623ff62dc1244f95b486041e35d685ef4e
Nick Drozd <nicholasdrozd@gmail.com>
2016-09-12 14:30:47 -0500

    commented out env stats

--- tally.c
+++ tally.c
@@ -8,15 +8,13 @@ int max_stack_depth = 0;
 
 /* env stats */
 
-int lookup_count = 0;
-int envs_traversed = 0;
-int frames_traversed = 0;
+// int lookup_count = 0;
+// int envs_traversed = 0;
+// int frames_traversed = 0;
 
 /* llh stats */
 
 int form_check_count = 0;
-// int isPrimitive_count = 0;
-// int isCompound_count = 0;
 
 /* stat functions */
 
@@ -28,13 +26,11 @@ void print_stats(void) {
 	printf("Total number of saves: %d", save_count);NL;
 	printf("Maximum stack depth: %d", max_stack_depth);NL;
 
-	printf("Total number of lookups: %d", lookup_count);NL;
-	printf("Total envs traversed: %d", envs_traversed);NL;
-	printf("Total frames traversed: %d", frames_traversed);NL;
+	// printf("Total number of lookups: %d", lookup_count);NL;
+	// printf("Total envs traversed: %d", envs_traversed);NL;
+	// printf("Total frames traversed: %d", frames_traversed);NL;
 
-	printf("Total times special forms checked: %d", form_check_count);NL;
-	// printf("Total times isPrimitive called: %d", isPrimitive_count);NL;
-	// printf("Total times isCompound called: %d", isCompound_count);NL;
+	printf("Total times syntax checked: %d", form_check_count);NL;
 
 	reset_stats();
 }
@@ -44,11 +40,9 @@ void reset_stats(void) {
 	curr_stack_depth = 0;
 	max_stack_depth = 0;
 
-	lookup_count = 0;
-	envs_traversed = 0;
-	frames_traversed = 0;
+	// lookup_count = 0;
+	// envs_traversed = 0;
+	// frames_traversed = 0;
 
 	form_check_count = 0;
-	// isPrimitive_count = 0;
-	// isCompound_count = 0;
 }
--- tally.h
+++ tally.h
@@ -14,12 +14,10 @@ extern int save_count;
 extern int curr_stack_depth;
 extern int max_stack_depth;
 
-extern int lookup_count;
-extern int envs_traversed;
-extern int frames_traversed;
+// extern int lookup_count;
+// extern int envs_traversed;
+// extern int frames_traversed;
 
 extern int form_check_count;
-// extern int isPrimitive_count;
-// extern int isCompound_count;
 
 #endif
