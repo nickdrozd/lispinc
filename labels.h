2c647971ee7a53418a2bda7f71493e4fe7057b42
Nick Drozd <nicholasdrozd@gmail.com>
2016-09-12 10:11:43 -0500

    separated labels from objects to prepare for compiled code

---
+++ labels.h
@@ -0,0 +1,20 @@
+/* there are more labels, 
+but these are the ones that 
+get saved and restored */
+
+typedef enum {
+	_DONE,
+	_IF_DECIDE,
+	_DID_ASS_VAL,
+	_DID_DEF_VAL,
+	_DID_FUNC,
+	_ACC_ARG,
+	_DID_LAST_ARG,
+	_SEQ_CONT,
+	_ALT_SEQ_CONT,
+
+
+	
+	label_count
+} Label;
+
--- objects.h
+++ objects.h
@@ -63,7 +63,7 @@
 #ifndef OBJECTS_GUARD
 #define OBJECTS_GUARD
 
-// #include "keywords.h"
+#include "labels.h"
 
 /* typedefs */
 
@@ -80,23 +80,6 @@ typedef struct Comp Comp;
 
 typedef struct Frame Frame;
 typedef struct Env Env;
-
-/* there are more labels, 
-but these are the ones that 
-get saved and restored */
-
-typedef enum {
-	_DONE,
-	_IF_DECIDE,
-	_DID_ASS_VAL,
-	_DID_DEF_VAL,
-	_DID_FUNC,
-	_ACC_ARG,
-	_DID_LAST_ARG,
-	_SEQ_CONT,
-	_ALT_SEQ_CONT,
-	label_count
-} Label;
 
 /* primitive functions */
 
